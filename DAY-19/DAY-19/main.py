b2855c7ecfb5795645edfbbbec020c5721920bd9
kanak chouksey <143768260+kanak8329@users.noreply.github.com>
2024-08-20 21:32:13 +0530

    Add files via upload

---
+++ DAY-19/DAY-19/main.py
@@ -0,0 +1,45 @@
+# from turtle import Turtle, Screen
+#
+# tim = Turtle()
+# screen = Screen()
+#
+# def move_forwards():
+#     tim.forward(10)
+#
+# screen.listen()
+# screen.onkey(key="space", fun=move_forwards)
+# screen.exitonclick()
+
+
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+def move_forwards():
+    tim.forward(10)
+
+def move_backwards():
+    tim.backward(10)
+
+def move_left():
+    new_heading = tim.heading() + 10
+    tim.setheading(new_heading)
+
+def move_right():
+    new_heading = tim.heading() - 10
+    tim.setheading(new_heading)
+
+def clear():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
+
+screen.listen()
+screen.onkey(move_forwards, "w")
+screen.onkey(move_backwards, "s")
+screen.onkey(move_left, "a")
+screen.onkey(move_right, "d")
+screen.onkey(clear, "c")  # Bind the clear function to the "c" key
+screen.exitonclick()
